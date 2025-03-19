68478a9ba6fe31bf65d09bfd3b85f4101d3dd467
Peter Tang <petersktang@gmail.com>
2025-03-19 23:09:44 +0800

    Initial Commit

---
+++ GraphNotesOld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GraphNotesOld
+//
+//  Created by Peter Tang on 19/3/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GraphNotesOldTests/GraphNotesOldTests.swift
@@ -0,0 +1,17 @@
+//
+//  GraphNotesOldTests.swift
+//  GraphNotesOldTests
+//
+//  Created by Peter Tang on 19/3/2025.
+//
+
+import Testing
+@testable import GraphNotesOld
+
+struct GraphNotesOldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
