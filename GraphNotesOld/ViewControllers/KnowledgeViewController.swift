c79c3cdd994ef24622001e6129b52de88bbe072b
Peter Tang <petersktang@gmail.com>
2025-03-23 01:27:51 +0800

    Knowledge layout depending on device orientation

--- GraphNotesOld/ViewControllers/KnowledgeViewController.swift
+++ GraphNotesOld/ViewControllers/KnowledgeViewController.swift
@@ -12,9 +12,35 @@ class KnowledgeViewController: UIViewController {
         super.viewDidLoad()
         configureSubviews()
     }
+    private var stackview: UIStackView?
 }
 extension KnowledgeViewController {
     func configureSubviews() {
-        
+        let question = UITextField(frame: .zero)
+        question.text = "Question"
+        question.backgroundColor = .systemYellow
+        question.font = UIFont.preferredFont(forTextStyle: .title3)
+        let answer = UITextView(frame: .zero)
+        answer.text = "Answer"
+        answer.layer.borderColor = UIColor.systemRed.cgColor
+        answer.font = UIFont.preferredFont(forTextStyle: .body)
+        answer.layer.borderWidth = 0.4
+        let stackview = UIStackView(arrangedSubviews: [question, answer])
+        stackview.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stackview)
+        NSLayoutConstraint.activate([
+            stackview.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+            stackview.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor),
+            stackview.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
+            stackview.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor)
+        ])
+        self.stackview = stackview
+        configureAxis()
+    }
+    override func willRotate(to toInterfaceOrientation: UIInterfaceOrientation, duration: TimeInterval) {
+        configureAxis()
+    }
+    func configureAxis() {
+        stackview?.axis = UIDevice.current.orientation.isPortrait ? .vertical : .horizontal
     }
 }
